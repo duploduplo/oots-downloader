e32ddfed1814bb2a9e8b328d7f7a18351f3f4414
Lorenzo Berni <duplo@develer.com>
2013-11-13 15:23:52 +0100

    Translate in english log messages and comments.

--- downloader.py
+++ downloader.py
@@ -26,9 +26,9 @@ def open_page(url):
     """
     Simpy download a content and return it as a string.
     """
-    log.debug('Richiesta pagina "%s"', url)
+    log.debug('Page requested: "%s"', url)
     page = urllib2.urlopen(url).read()
-    log.debug('Download di "%s" terminato', url)
+    log.debug('"%s": download finished', url)
     return page
 
 def get_range(homepage='http://www.giantitp.com/Comics.html', directory='.'):
@@ -36,27 +36,26 @@ def get_range(homepage='http://www.giantitp.com/Comics.html', directory='.'):
     Return the latest downloaded comic and the latest available.
     """
     def _latest(homepage):
-        log.debug('Ricerco l\'indice dell\'ultima vignetta in %s', homepage)
+        log.debug('Finding latest downloaded comic in %s', homepage)
         p = open_page(homepage)
-        # Prende il link all'ultima vignetta, dalla homepage per calcolare
-        # l'indice.
+        # Find the latest comic index into the homepage.
         try:
             _l = int(re.search(r'<A href="/comics/oots(\d+)\.html" class="SideBar">', p).group(1))
-            log.info('Indice dell\'ultima vignetta trovata: %d', _l)
+            log.info('Latest available comic index: %d', _l)
             return _l
         except:
-            log.error('Si è verificato un errore durante la ricerca della url dell\'ultima vignetta')
+            log.error('Error: can\'t find the latest comic url')
             raise
 
     def _last_downloaded(directory):
-        log.debug('Ricerco l\'indice dell\'ultima vignetta scaricata')
+        log.debug('Seraching for the latest downloaded comic index')
         try:
             _downloaded = filter(lambda x: _img_filename.match(x), os.listdir(directory))[-1]
             _downloaded = int(_img_filename.match(_downloaded).group(1))
-            log.info('L\'ultima vignetta scaricata è la numero %d', _downloaded)
+            log.info('Latest downloaded comic index: %d', _downloaded)
         except IndexError:
             _downloaded = 0
-            log.info('Nessuna immagine scaricata precedentemente: parto dalla prima.')
+            log.info('No comic is available, starting from the first one')
         return _downloaded
 
     return _last_downloaded(directory), _latest(homepage)
@@ -72,18 +71,18 @@ def get_image(n):
         return 'http://www.giantitp.com' + _img_url_regex.search(page).group(1)
 
     img_url = _get_img_url(_get_comic_page(n))
-    log.info('Inizio il download di "%s"', img_url)
+    log.info('Downloading "%s"', img_url)
     img = open_page(img_url)
-    log.info('Download terminato con successo')
+    log.info('Download finished')
     return img
 
 def save_image(img, filename):
     """
     Save the image into file "filename".
     """
-    log.info('Inizio la scrittura di %s', filename)
+    log.info('Writing %s', filename)
     open(filename, 'w').write(img)
-    log.info('Scrittura di %s terminata con successo', filename)
+    log.info('%s written', filename)
 
 
 def main():
